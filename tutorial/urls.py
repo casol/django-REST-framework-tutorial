3ef8de7895b1be79a664b1376e6399c6078b797b
casol <casoltys@gmail.com>
2018-04-16 19:23:28 +0200

    Add a schema
    REST framework supports either explicitly defined schema views, or automatically generated schemas.
    Since we're using viewsets and routers, we can simply use the automatic schema generation.

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url, include
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include('snippets.urls')),
 ]
